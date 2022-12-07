c474cfb9ff3c02d516976341ceb3c60185fe43cc
nervevau2 <eugenshow83@gmail.com>
2022-12-07 12:26:47 +0200

    use hooks

--- src/components/charList/charList.js
+++ src/components/charList/charList.js
@@ -14,7 +14,6 @@ const CharList = (props) => {
     const[charEnded, setCharEnded] = useState(false);
 
     const service = new MarvelService();
-
     let itemRefs = useRef([]);
 
     useEffect(() => onRequest(),[])
