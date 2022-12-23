5bc0cdc85fe89447b05e21dfa7e3570a4f625975
Eugeniu Ivanes <65826461+EugenIvanes@users.noreply.github.com>
2022-12-23 14:15:13 +0200

    Delete src directory

--- src/components/pages/index.js
+++
@@ -1,6 +0,0 @@
-import MainPage from "./MaingPage";
-import Comics from "./Comics";
-import Page404 from "./404";
-import SingleComicPage from "./SingleComicPage";
-
-export{MainPage, Comics, Page404, SingleComicPage};
